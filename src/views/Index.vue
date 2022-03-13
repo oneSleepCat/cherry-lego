0a5213ea25544079a23b442e6dbc21a50fd05aed
xiaoya <2876379929@qq.com>
2022-03-14 02:04:52 +0800

    feat: 测试

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,7 +3,7 @@
     <a-layout :style="{ background: '#fff' }">
       <a-layout-header class="header">
         <div class="page-title">
-          <router-link to="/">慕课乐高</router-link>
+          <router-link to="/">乐高</router-link>
         </div>
         <user-profile :user="user" />
       </a-layout-header>
